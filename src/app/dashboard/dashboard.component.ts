2ef9e061c395ffdb19aff97c04e01adcc203a0a2
Siddhant Gupta <siddhantgupta384@gmail.com>
2023-12-02 23:01:22 +0530

    Filters Added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,17 +12,32 @@ import { data } from '../../environments/environment.development';
 })
 export class DashboardComponent implements OnInit, AfterViewInit  {
   data: any;
+  filteredData: any;
   yOptions: string[] =[];
   selectedYOption: string = 'intensity';
-  graphType: string[] = ['Bar Chart' , "Scatter Plot"];
+  xOptions: string[] =[];
+  selectedXOption: string = 'intensity';
+  graphType: string[] = ['Bar Chart' , "Scatter Plot", "Pie Chart" , "Area Chart"];
   selectedGraphType: string = 'Bar Chart';
   svg:any;
   margin:any = { top: 20, right: 20, bottom: 30, left: 40 };
   width:any = 600 - this.margin.left - this.margin.right;
   height:any = 400 - this.margin.top - this.margin.bottom;
 
+  // Filter variables
+  endYearFilter: string | null = null;
+  topicsFilter: string | null = null;
+  sectorFilter: string | null = null;
+  regionFilter: string | null = null;
+  pestFilter: string | null = null;
+  sourceFilter: string | null = null;
+  swotFilter: string | null = null;
+
+  // FIX_ME Pie chart will have only one attribute 
+
   ngOnInit() {
     this.data = data
+    this.filteredData = data
     this.extractYOptions()
     this.createSvg()
   }
@@ -32,16 +47,26 @@ export class DashboardComponent implements OnInit, AfterViewInit  {
     this.createBarChart();
   }
 
-  onYOptionChange() {
-    switch(this.selectedGraphType){
-      case "Bar Chart":
-        this.createBarChart();
-        break;
-      case "Scatter Plot":
-        this.createScatterPlot();
-    }
-  }  
-  
+  applyFilters() {
+    // Apply filters to the data based on selected filter values
+    this.filteredData  = this.data.filter((item:any) => {
+      return (
+        (this.endYearFilter === null || item.end_year === this.endYearFilter) &&
+        (this.topicsFilter === null || item.topic === this.topicsFilter) &&
+        (this.sectorFilter === null || item.sector === this.sectorFilter) &&
+        (this.regionFilter === null || item.region === this.regionFilter) &&
+        (this.pestFilter === null || item.pestle === this.pestFilter) &&
+        (this.sourceFilter === null || item.source === this.sourceFilter) &&
+        (this.swotFilter === null || item.swot === this.swotFilter)
+      );
+    });
+    this.createBarChart();
+  }
+
+  getUniqueValues(key: string): string[] {
+    return Array.from(new Set(this.data.map((item:any) => item[key])));
+  }
+
   onGraphTypeChange() {
     switch(this.selectedGraphType){
       case "Bar Chart":
@@ -49,15 +74,24 @@ export class DashboardComponent implements OnInit, AfterViewInit  {
         break;
       case "Scatter Plot":
         this.createScatterPlot();
+        break;
+      case "Pie Chart":
+        this.createPieChart();
+        break;
+      case "Area Chart":
+        this.createAreaChart();
+        break;
+        
     }
   }
 
   private extractYOptions() {
-    if (this.data.length > 0) {
+    if (this.filteredData.length > 0) {
       // Extract keys from the first data item as yOptions
-      this.yOptions = Object.keys(this.data[0]);
+      this.yOptions = Object.keys(this.filteredData[0]);
+      this.xOptions = Object.keys(this.filteredData[0]);
       // Remove the keys that are not numeric (assuming these are your y-axis options)
-      this.yOptions = this.yOptions.filter((key) => typeof this.data[0][key] === 'number');
+      this.yOptions = this.yOptions.filter((key) => typeof this.filteredData[0][key] === 'number');
     }
   }
 
@@ -72,51 +106,6 @@ export class DashboardComponent implements OnInit, AfterViewInit  {
       .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
   }
 
-
-  createScatterPlot() {
-
-    // Clear the existing content of the x-axis and y-axis
-    this.svg.selectAll('*').remove();
-
-    const xScale:any = d3.scaleLinear().range([0, this.width]);
-    const yScale:any = d3.scaleLinear().range([this.height, 0]);
-    const sizeScale:any = d3.scaleLinear().range([5, 15]);
-
-    xScale.domain([0, d3.max(this.data, (d:any) => d.intensity)]);
-    yScale.domain([0, d3.max(this.data, (d:any) => d[this.selectedYOption])]);
-    sizeScale.domain([0, 50]);
-
-    this.svg.selectAll('circle')
-      .data(this.data)
-      .enter()
-      .append('circle')
-      .attr('cx', (d:any) => xScale(d.intensity))
-      .attr('cy', (d:any) => yScale(d[this.selectedYOption]))
-      .attr('r', (d:any) => sizeScale(1))
-      .attr('fill', 'blue');
-
-    // Add axes
-    this.svg.append('g')
-      .attr('transform', `translate(0,${this.height})`)
-      .call(d3.axisBottom(xScale));
-    this.svg.append('g').call(d3.axisLeft(yScale));
-
-    // Add labels
-    this.svg.append('text')
-      .attr('transform', `translate(${this.width / 2},${this.height + this.margin.top + 10})`)
-      .style('text-anchor', 'middle')
-      .text('Sectors');
-
-    this.svg.append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 0 - this.margin.left)
-      .attr('x', 0 - this.height / 2)
-      .attr('dy', '1em')
-      .style('text-anchor', 'middle')
-      .text(this.selectedYOption);
-  
-    }
-
   createBarChart() {
 
     // Clear the existing content of the x-axis and y-axis
@@ -125,14 +114,14 @@ export class DashboardComponent implements OnInit, AfterViewInit  {
     const xScale:any = d3.scaleBand().range([0, this.width]).padding(0.1);
     const yScale:any = d3.scaleLinear().range([this.height, 0]);
 
-    xScale.domain(this.data.map((d:any) => d.sector));
-    yScale.domain([0, d3.max(this.data, (d:any) => d[this.selectedYOption])]);
+    xScale.domain(this.filteredData.map((d:any) => d[this.selectedXOption]));
+    yScale.domain([0, d3.max(this.filteredData, (d:any) => d[this.selectedYOption])]);
 
     this.svg.selectAll('rect')
-      .data(this.data)
+      .data(this.filteredData)
       .enter()
       .append('rect')
-      .attr('x', (d:any) => xScale(d.sector))
+      .attr('x', (d:any) => xScale(d[this.selectedXOption]))
       .attr('y', (d:any) => yScale(d[this.selectedYOption]))
       .attr('width', xScale.bandwidth())
       .attr('height', (d:any) => this.height - yScale(d[this.selectedYOption]))
@@ -152,7 +141,7 @@ export class DashboardComponent implements OnInit, AfterViewInit  {
     this.svg.append('text')
       .attr('transform', `translate(${this.width / 2},${this.height + this.margin.top + 40})`)
       .style('text-anchor', 'middle')
-      .text('Sectors');
+      .text(this.selectedXOption);
 
     this.svg.append('text')
       .attr('transform', 'rotate(-90)')
@@ -161,6 +150,121 @@ export class DashboardComponent implements OnInit, AfterViewInit  {
       .attr('dy', '1em')
       .style('text-anchor', 'middle')
       .text(this.selectedYOption);
+  }
+
+  createScatterPlot() {
+
+    // Clear the existing content of the x-axis and y-axis
+    this.svg.selectAll('*').remove();
+
+    const xScale:any = d3.scaleLinear().range([0, this.width]);
+    const yScale:any = d3.scaleLinear().range([this.height, 0]);
+    const sizeScale:any = d3.scaleLinear().range([5, 15]);
+
+    xScale.domain([0, d3.max(this.filteredData, (d:any) => d[this.selectedXOption])]);
+    yScale.domain([0, d3.max(this.filteredData, (d:any) => d[this.selectedYOption])]);
+    sizeScale.domain([0, 50]);
+
+    this.svg.selectAll('circle')
+      .data(this.filteredData)
+      .enter()
+      .append('circle')
+      .attr('cx', (d:any) => xScale(d[this.selectedXOption]))
+      .attr('cy', (d:any) => yScale(d[this.selectedYOption]))
+      .attr('r', (d:any) => sizeScale(1))
+      .attr('fill', 'blue');
+
+    // Add axes
+    this.svg.append('g')
+      .attr('transform', `translate(0,${this.height})`)
+      .call(d3.axisBottom(xScale));
+    this.svg.append('g').call(d3.axisLeft(yScale));
+
+    // Add labels
+    this.svg.append('text')
+      .attr('transform', `translate(${this.width / 2},${this.height + this.margin.top + 10})`)
+      .style('text-anchor', 'middle')
+      .text(this.selectedXOption);
+
+    this.svg.append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 0 - this.margin.left)
+      .attr('x', 0 - this.height / 2)
+      .attr('dy', '1em')
+      .style('text-anchor', 'middle')
+      .text(this.selectedYOption);
+  
+  }
+  
+
+  createPieChart() {
+    // Clear the existing content of the SVG
+    this.svg.selectAll('*').remove();
+  
+    const radius = Math.min(this.width, this.height) / 2;
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+  
+    const pie = d3.pie().value((d: any) => d[this.selectedYOption]);
+  
+    const arc = d3.arc().outerRadius(radius - 10).innerRadius(0);
+  
+    const arcs = this.svg
+      .selectAll('arc')
+      .data(pie(this.filteredData))
+      .enter()
+      .append('g')
+      .attr('class', 'arc')
+      .attr('transform', `translate(${this.width / 2},${this.height / 2})`);
+  
+    arcs
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', (d: any) => colorScale(d.data.sector));
+  
+    // Optionally add labels to the pie chart
+    arcs
+      .append('text')
+      .attr('transform', (d: any) => `translate(${arc.centroid(d)})`)
+      .attr('dy', '0.35em')
+      .text((d: any) => d.data.sector);
+  }
+  createAreaChart() {
+    // Clear the existing content of the SVG
+    this.svg.selectAll('*').remove();
+  
+    const xScale: any = d3.scaleLinear().range([0, this.width]);
+    const yScale: any = d3.scaleLinear().range([this.height, 0]);
+  
+    xScale.domain(d3.extent(this.filteredData, (d: any) => d[this.selectedXOption]));
+    yScale.domain([0, d3.max(this.filteredData, (d: any) => d[this.selectedYOption])]);
+  
+    const area = d3
+      .area()
+      .x((d: any) => xScale(d[this.selectedXOption]))
+      .y0(this.height)
+      .y1((d: any) => yScale(d[this.selectedYOption]));
+  
+    this.svg.append('path').datum(this.filteredData).attr('class', 'area').attr('d', area);
+  
+    // Add axes
+    this.svg.append('g').attr('transform', `translate(0,${this.height})`).call(d3.axisBottom(xScale));
+    this.svg.append('g').call(d3.axisLeft(yScale));
+  
+    // Add labels
+    this.svg
+      .append('text')
+      .attr('transform', `translate(${this.width / 2},${this.height + this.margin.top + 10})`)
+      .style('text-anchor', 'middle')
+      .text(this.selectedYOption);
+  
+    this.svg
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 0 - this.margin.left)
+      .attr('x', 0 - this.height / 2)
+      .attr('dy', '1em')
+      .style('text-anchor', 'middle')
+      .text(this.selectedXOption);
   }
   
 }
